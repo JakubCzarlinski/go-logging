ea4f06036bd25e34dca0585e3facfce71b693da9
Jakub Czarlinski <jakubczarlinski@gmail.com>
2024-10-13 23:38:43 +0100

    Add more formats.

--- colours.go
+++ colours.go
@@ -1,6 +1,7 @@
 package logging
 
 const reset = "\033[0m"
+const resetBg = "\033[49m"
 
 const (
 	bgGreen   = "\033[97;42m"
@@ -22,31 +23,31 @@ const (
 )
 
 func GreenBg(msg string) string {
-	return WrapInColour(msg, bgGreen)
+	return WrapInColourBg(msg, bgGreen)
 }
 
 func WhiteBg(msg string) string {
-	return WrapInColour(msg, bgWhite)
+	return WrapInColourBg(msg, bgWhite)
 }
 
 func YellowBg(msg string) string {
-	return WrapInColour(msg, bgYellow)
+	return WrapInColourBg(msg, bgYellow)
 }
 
 func RedBg(msg string) string {
-	return WrapInColour(msg, bgRed)
+	return WrapInColourBg(msg, bgRed)
 }
 
 func BlueBg(msg string) string {
-	return WrapInColour(msg, bgBlue)
+	return WrapInColourBg(msg, bgBlue)
 }
 
 func MagentaBg(msg string) string {
-	return WrapInColour(msg, bgMagenta)
+	return WrapInColourBg(msg, bgMagenta)
 }
 
 func CyanBg(msg string) string {
-	return WrapInColour(msg, bgCyan)
+	return WrapInColourBg(msg, bgCyan)
 }
 
 func Green(msg string) string {
@@ -79,4 +80,61 @@ func Cyan(msg string) string {
 
 func WrapInColour(msg string, colour string) string {
 	return colour + msg + reset
+}
+
+func WrapInColourBg(msg string, colourBg string) string {
+	return colourBg + msg + resetBg
+}
+
+func Bold(msg string) string {
+	return "\033[1m" + msg + "\033[22m"
+}
+
+func Dim(msg string) string {
+	return "\033[2m" + msg + "\033[22m"
+}
+
+func Italic(msg string) string {
+	return "\033[3m" + msg + "\033[23m"
+}
+
+func Underline(msg string) string {
+	return "\033[4m" + msg + "\033[24m"
+}
+
+func Blink(msg string) string {
+	return "\033[5m" + msg + "\033[25m"
+}
+
+func FastBlink(msg string) string {
+	return "\033[6m" + msg + "\033[25m"
+}
+
+func Reverse(msg string) string {
+	return "\033[7m" + msg + "\033[27m"
+}
+
+func Hidden(msg string) string {
+	return "\033[8m" + msg + "\033[28m"
+}
+
+func StrikeThrough(msg string) string {
+	return "\033[9m" + msg + "\033[29m"
+}
+
+func DoubleUnderline(msg string) string {
+	return "\033[21m" + msg + "\033[24m"
+}
+
+func Overlined(msg string) string {
+	return "\033[53m" + msg + "\033[55m"
+}
+
+func Combine(formats ...ColourFunc) ColourFunc {
+	return func(msg string) string {
+		for _, format := range formats {
+			msg = format(msg)
+		}
+		return msg
+	}
 }
